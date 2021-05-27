a000dc6b0eb995c6de2d30ba21aadbab700d36d5
Error4ik <fearerror@yandex.ru>
2021-05-28 00:15:29 +0300

    Filled login component.

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../../services/auth.service';
+import {TokenService} from '../../../services/token.service';
+import {NotificationService} from '../../../services/notification.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  public loginForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService,
+    private notificationService: NotificationService,
+    private router: Router,
+    private formBuilder: FormBuilder) {
+    if (this.tokenService.getUser()) {
+      this.router.navigate(['main']);
+    }
   }
 
+  ngOnInit(): void {
+    this.loginForm = this.createLoginForm();
+  }
+
+  createLoginForm(): FormGroup {
+    return this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  submit(): void {
+    this.authService.loginUser({
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
+    }).subscribe(data => {
+      console.log(data);
+      this.tokenService.saveToken(data.token);
+      this.tokenService.saveUser(data);
+      this.notificationService.showSnackBar('Successfully logged in.');
+      this.router.navigate(['/']);
+      window.location.reload();
+    }, error => {
+      console.log(error);
+      this.notificationService.showSnackBar(error.message);
+    });
+  }
 }
