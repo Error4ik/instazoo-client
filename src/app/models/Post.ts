795c60bab4535176086917669ca56fc8cd58f8f4
Error4ik <fearerror@yandex.ru>
2021-05-29 13:47:23 +0300

    Fixed fields name title and usersLiked.

--- src/app/models/Post.ts
+++ src/app/models/Post.ts
@@ -2,12 +2,12 @@ import {Comment} from './Comment';
 
 export interface Post {
   id?: number;
-  tittle: string;
+  title: string;
   username?: string;
   caption: string;
   location: string;
   image?: File;
   likes?: number;
-  userLiked?: string[];
+  likedUsers?: string[];
   comments?: Comment[];
 }
